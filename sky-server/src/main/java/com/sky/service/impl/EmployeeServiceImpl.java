9c6c6c71aa09af018e70cf83d76f2001a869756d
Philosophy9420 <166003652@qq.com>
2026-01-25 23:17:57 +0800

    day1 实现新增员工功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +58,36 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 将新员工数据插入到数据库当中
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+//        将从前端接受都数据传输对象转换为插入数据库的对象
+        Employee employee = new Employee();
+
+//        使用spring提供都工具类实现属性复制
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+//      设置密码 默认为123456 定义在常量类当中
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+//        设置账号状态 默认为1 表示可用，0为不可用
+        employee.setStatus(StatusConstant.ENABLE);
+
+//        设置创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+//        设置创建该用户的当前用户Id
+        //TODO 之后需要修改成当前用户的实际id
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+//        调用持久层接口，将数据持久化到数据库当中
+        employeeMapper.insert(employee);
     }
 
 }
