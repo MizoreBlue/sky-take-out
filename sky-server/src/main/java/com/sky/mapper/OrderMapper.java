4db440f49aac99d5cfd1f14540992f22cd6e989e
MizoreBlue <MizoreBlue@qq.com>
2026-03-20 15:57:28 +0800

    day9 C-用户支付业务代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
@@ -12,4 +13,20 @@ public interface OrderMapper {
      * @param orders entity
      */
     void insert(Orders orders);
+
+
+    /**
+     * 根据订单号查订单
+     * @param outTradeNo
+     * @return
+     */
+    @Select("select * from orders where number = #{ouTradeNo}")
+    Orders getByNumber(String outTradeNo);
+
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 
 public interface OrderService {
@@ -11,4 +13,20 @@ public interface OrderService {
      * @return Vo
      */
     OrderSubmitVO orderSubmit(OrdersSubmitDTO ordersSubmitDTO);
+
+
+    /**
+     * 订单支付成功
+     * @param orderNumber string
+     */
+    void paySuccess(String orderNumber);
+
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
 }
